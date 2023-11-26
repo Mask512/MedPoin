6e994d264b8c2fd435ee8a1ef67ffd2e2fda9f36
Mask512 <Mask.roktz@gmail.com>
2023-11-27 04:51:31 +0700

    improve responsive and code

--- src/scripts/components/patient-cards.js
+++ src/scripts/components/patient-cards.js
@@ -18,41 +18,21 @@ class PatientCards extends HTMLElement {
 
     this.innerHTML = `
       <div class="flex flex-wrap gap-4 ${animatePulseClass}">
-  
-        <div
-          class="flex-auto sm:flex-none grid grid-cols-[2fr_1fr] grid-rows-2 place-items-start rounded-md bg-gray-300 p-4 font-semibold text-gray-700 ring-1 ring-gray-400 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <h3 class="text-xl">Pasien Hari ini</h3>
-          <p class="col-start-1 row-start-2 text-4xl"> ${this._totalPasien}
-          </p>
-          <span class="material-symbols-outlined col-start-2 row-span-3 text-7xl">
-            group
-          </span>
-        </div>
-  
-        <div
-          class="flex-auto sm:flex-none grid grid-cols-[2fr_1fr] grid-rows-2 place-items-start rounded-md bg-gray-50 p-4 font-semibold text-gray-700 ring-1 ring-gray-400 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <h3 class="text-xl">Belum Terlayani</h3>
-          <p class="col-start-1 row-start-2 text-4xl">
-          ${this._belumTerlayani}
-          </p>
-          <span class="material-symbols-outlined col-start-2 row-span-3 text-7xl">
-            group
-          </span>
-        </div>
-  
-        <div
-          class="flex-auto sm:flex-none grid grid-cols-[2fr_1fr] grid-rows-2 place-items-start rounded-md bg-gray-50 p-4 font-semibold text-gray-700 ring-1 ring-gray-400 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <h3 class="text-xl">Terlayani</h3>
-          <p class="col-start-1 row-start-2 text-4xl">
-          ${this._terlayani}
-          </p>
-          <span class="material-symbols-outlined col-start-2 row-span-3 text-7xl">
-            group
-          </span>
-        </div>
+        ${this.renderCard('Pasien Hari ini', this._totalPasien, 'bg-gray-300')}
+        ${this.renderCard('Belum Terlayani', this._belumTerlayani, 'bg-gray-50')}
+        ${this.renderCard('Terlayani', this._terlayani, 'bg-gray-50')}
+      </div>
+    `;
+  }
+
+  renderCard(title, value, cardColor) {
+    return `
+      <div class="flex-auto sm:flex-none grid grid-cols-[2fr_1fr] grid-rows-2 place-items-start rounded-md p-4 font-semibold text-gray-700 ring-1 ring-gray-400 dark:bg-gray-800 dark:text-gray-300 ${cardColor}">
+        <h3 class="text-xl">${title}</h3>
+        <p class="col-start-1 row-start-2 text-4xl">${value || ''}</p>
+        <span class="material-symbols-outlined col-start-2 row-span-3 text-7xl">
+          group
+        </span>
       </div>
     `;
   }
