786b8f2d474f2c71d65f8e8c99b925edbc23009c
Mask512 <Mask.roktz@gmail.com>
2023-11-30 14:01:20 +0700

    change input phone number style

--- src/scripts/components/registration-form.js
+++ src/scripts/components/registration-form.js
@@ -185,19 +185,15 @@ class RegistrationForm extends HTMLElement {
         </div>
 
         <div>
-          <label
-            for="nomor-hp"
-            class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-            >Nomor Handphone</label
-          >
-          <input
-            type="number"
-            name="nomor-hp"
-            id="nomor-hp"
-            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="081xxxxxxx"
-            required=""
-          />
+          <label for="nomor-hp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor Handphone</label>
+          <div class="relative">
+              <div class="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
+                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
+                      <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
+                  </svg>
+              </div>
+              <input type="text" id="nomor-hp" name="nomor_hp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0812345678" required>
+          </div>
         </div>
 
         <div>
@@ -216,19 +212,15 @@ class RegistrationForm extends HTMLElement {
         </div>
 
         <div>
-          <label
-            for="nomor-hp-keluarga"
-            class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-            >Nomor Handphone Keluarga</label
-          >
-          <input
-            type="number"
-            name="nomor-hp-keluarga"
-            id="nomor-hp-keluarga"
-            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="081xxxxxxx"
-            required=""
-          />
+          <label for="nomor-hp-keluarga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor Handphone Keluarga</label>
+          <div class="relative">
+              <div class="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
+                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
+                      <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
+                  </svg>
+              </div>
+              <input type="text" id="nomor-hp-keluarga" name="nomor_hp_keluarga" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0812345678" required>
+          </div>
         </div>
       </div>
       <div class="flex flex-wrap gap-4">
