d7b8214085a2bebe36c686555fc682c654da7ae8
Mask512 <Mask.roktz@gmail.com>
2023-11-30 15:21:42 +0700

    improve naming item to title

--- src/scripts/components/side-bar.js
+++ src/scripts/components/side-bar.js
@@ -33,7 +33,7 @@ class SideBar extends HTMLElement {
         <li class="menu-item group">
           <a href=#${menu.link} class="group flex items-start gap-4 rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
             <span class="material-symbols-outlined h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white group-[.active]:text-gray-900 dark:group-[.active]:text-white" aria-hidden="true">${menu.icon}</span>
-            <span>${menu.item}</span>
+            <span>${menu.title}</span>
           </a>
         </li>
       `,
