3794d8c790642a02a1a957e06e767022852481eb
Mask512 <Mask.roktz@gmail.com>
2023-12-13 04:43:51 +0700

    refactor fetch api data

--- src/scripts/data/data.js
+++ src/scripts/data/data.js
@@ -9,191 +9,116 @@ const BASE_OPTIONS = {
 };
 
 class DATA {
-  static async getPatients() {
-    const response = await fetch(API_ENDPOINT.PATIENTS, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
-  }
+  static async fetchData(endpoint, method = 'GET', body = null) {
+    const options = { ...BASE_OPTIONS, method };
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
 
-  static async getDoctors() {
-    const response = await fetch(API_ENDPOINT.DOCTORS, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
-  }
+    const response = await fetch(endpoint, options);
 
-  static async getNurses() {
-    const response = await fetch(API_ENDPOINT.NURSES, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
-  }
-
-  static async getAdmins() {
-    const response = await fetch(API_ENDPOINT.ADMIN, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
-  }
-
-  static async getUsers() {
-    const response = await fetch(API_ENDPOINT.USERS, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
-  }
-
-  static async getAnamnesis(number) {
-    const response = await fetch(`${API_ENDPOINT.ANAMNESIS}?no_rawat=${number}`, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
-  }
-
-  static async dashboard() {
-    const response = await fetch(API_ENDPOINT.DASHBOARD, {
-      ...BASE_OPTIONS,
-    });
     if (response.status === 204) {
       return {
         error: true,
-        message: 'Belum ada pasien yang didaftarkan hari ini',
+        message: 'Belum ada data yang ditemukan',
         data: null,
       };
     }
+
     return response.json();
+  }
+
+  static async getPatients() {
+    return DATA.fetchData(API_ENDPOINT.PATIENTS);
+  }
+
+  static async getDoctors() {
+    return DATA.fetchData(API_ENDPOINT.DOCTORS);
+  }
+
+  static async getNurses() {
+    return DATA.fetchData(API_ENDPOINT.NURSES);
+  }
+
+  static async getAdmins() {
+    return DATA.fetchData(API_ENDPOINT.ADMIN);
+  }
+
+  static async getUsers() {
+    return DATA.fetchData(API_ENDPOINT.USERS);
+  }
+
+  static async getAnamnesis(number) {
+    return DATA.fetchData(`${API_ENDPOINT.ANAMNESIS}?no_rawat=${number}`);
+  }
+
+  static async dashboard() {
+    return DATA.fetchData(API_ENDPOINT.DASHBOARD);
   }
 
   static async icd9() {
-    const response = await fetch(API_ENDPOINT.ICD9, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.ICD9);
   }
 
   static async icd10() {
-    const response = await fetch(API_ENDPOINT.ICD10, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.ICD10);
   }
 
   static async signIn(id, password) {
-    const data = { id, password };
-    const response = await fetch(API_ENDPOINT.SIGN_IN, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify(data),
-    });
-    return response.json();
+    const body = { id, password };
+    return DATA.fetchData(API_ENDPOINT.SIGN_IN, 'POST', body);
   }
 
   static async signOut() {
-    const response = await fetch(API_ENDPOINT.SIGN_OUT, {
-      ...BASE_OPTIONS,
-      method: 'DELETE',
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.SIGN_OUT, 'DELETE');
   }
 
   static async registerAdmin(id, nama) {
-    const response = await fetch(API_ENDPOINT.REG_ADMIN, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify({ id, nama }),
-    });
-    return response.json();
+    const body = { id, nama };
+    return DATA.fetchData(API_ENDPOINT.REG_ADMIN, 'POST', body);
   }
 
   static async registerDoctor(id, nama, spesialis) {
-    const response = await fetch(API_ENDPOINT.REG_DOCTOR, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify({ id, nama, spesialis }),
-    });
-    return response.json();
+    const body = { id, nama, spesialis };
+    return DATA.fetchData(API_ENDPOINT.REG_DOCTOR, 'POST', body);
   }
 
   static async registerNurse(id, nama) {
-    const response = await fetch(API_ENDPOINT.REG_NURSES, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify({ id, nama }),
-    });
-    return response.json();
+    const body = { id, nama };
+    return DATA.fetchData(API_ENDPOINT.REG_NURSES, 'POST', body);
   }
 
   static async registerUser(id, password, role) {
-    const response = await fetch(API_ENDPOINT.REG_USER, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify({
-        id,
-        password,
-        role,
-      }),
-    });
-    return response.json();
+    const body = { id, password, role };
+    return DATA.fetchData(API_ENDPOINT.REG_USER, 'POST', body);
   }
 
   static async registerPatient(form) {
-    const response = await fetch(API_ENDPOINT.REG_PATIENT, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify(form),
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.REG_PATIENT, 'POST', form);
   }
 
   static async patientAppointment(form) {
-    const response = await fetch(API_ENDPOINT.PATIENTS_APPOINTMENT, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify(form),
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.PATIENTS_APPOINTMENT, 'POST', form);
   }
 
   static async anamnesis(form) {
-    const response = await fetch(API_ENDPOINT.ANAMNESIS, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify(form),
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.ANAMNESIS, 'POST', form);
   }
 
   static async exam(form) {
-    const response = await fetch(API_ENDPOINT.EXAM, {
-      ...BASE_OPTIONS,
-      method: 'POST',
-      body: JSON.stringify(form),
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.EXAM, 'POST', form);
   }
 
   static async patientNumber() {
-    const response = await fetch(API_ENDPOINT.RM_NUMBER, {
-      ...BASE_OPTIONS,
-      method: 'GET',
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.RM_NUMBER, 'GET');
   }
 
   static async medicalRecords() {
-    const response = await fetch(API_ENDPOINT.MED_RECORDS, {
-      ...BASE_OPTIONS,
-      method: 'GET',
-    });
-    return response.json();
+    return DATA.fetchData(API_ENDPOINT.MED_RECORDS, 'GET');
   }
 
   static async medicalRecordsDetail(number) {
-    const response = await fetch(`${API_ENDPOINT.MED_RECORDS}/detail?no_rm=${number}`, {
-      ...BASE_OPTIONS,
-    });
-    return response.json();
+    return DATA.fetchData(`${API_ENDPOINT.MED_RECORDS}/detail?no_rm=${number}`);
   }
 }
 
